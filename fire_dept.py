74d59ebf4d96a9d92c5a652934bd059186455a77
Aletheia Cooper <aletheiacooper@gmail.com>
2021-06-12 13:51:43 +0000

    Caching initial data pull

--- fire_dept.py
+++ fire_dept.py
@@ -7,28 +7,24 @@ import sys
 import os
 
 file_name = sys.argv[1]
+url = "https://data.sfgov.org/resource/RowID.json"
+header = {"$$app_token" : "I6QzPzDfpk4SrU7Eo0PBYhyiT"}
 
 if os.path.exists(file_name):
     with open(file_name, "r") as data_file:
-        response = json.load(data_file)
-
+        initial_data = json.load(data_file)
 else:
 
-    response = requests.get("https://data.sfgov.org/resource/RowID.json")
     # In production environment, would do error checking in case the data doesn't load properly
     
-    json_string = json.dumps(response.json())
-    fire_dept_data = json.loads(json_string)
-    
-    # Only loads the first 1000 rows.  Let's see if we can do better.
-    
-    url = "https://data.sfgov.org/resource/RowID.json"
-    header = {"$$app_token" : "I6QzPzDfpk4SrU7Eo0PBYhyiT"}
     params = {"$limit" : 60000}
     response = requests.get(url, headers=header, params=params)
-
+    
     with open(file_name, "w") as output_file:
         json.dump(response.json(), output_file)
+
+    initial_data = json.loads(json.dumps(response.json()))
+
 
 print("Finished pulling data")
 
@@ -48,7 +44,7 @@ def load_from_response_to_dict(response):
     json_string = json.dumps(response.json())
     return json.loads(json_string)
 
-fire_dept_data_first_60000 = load_from_response_to_dict(response)
+fire_dept_data_first_60000 = initial_data
 before_2010_in_first_60k = len([thing for thing in fire_dept_data_first_60000 if thing["call_date"].find("200") != -1])
 
 print("Total number of rows pulled: " + str(len(fire_dept_data_first_60000)))
